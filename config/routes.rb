4fa7b11aa7984c706d6ee2c418f2ec030d35d7a8
Brandon Green <brandon@brandonleeproductions.com>
2019-03-26 15:50:53 -0600

    test different types of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,32 @@
 Rails.application.routes.draw do
-  get 'demo/index'
+  root 'demo#index' #root route
+  
+  
+  get 'demo/index' #Simple/match route
+
+  # default route
+  # may go away in future rails release
+  # get ':controller(/:action(/:id))'
+  
+  
+  # match "demo/index", :to => "demo#index", :via => :get #longer version of the simple route
+
+=begin 
+  Default Route Structure
+
+  :controller/:action/:id 
+
+  StudentsController, edit action, id = 52
+
+  get ':controller(/:action(/:id))'
+
+  match ':controller(/:action(/:id))', :via => :get
+=end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+
 end
